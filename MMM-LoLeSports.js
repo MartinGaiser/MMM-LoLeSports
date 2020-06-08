b4b7995ff3c5eb1f258af16e01bc91648c4b3440
Martin Gaiser <martin-gaiser@t-online.de>
2020-06-08 12:58:19 +0200

    created more classes and changed styling

--- MMM-LoLeSports.js
+++ MMM-LoLeSports.js
@@ -1,3 +1,5 @@
+
+
 Module.register("MMM-LoLeSports",{
 
     getDom: function() {
@@ -6,45 +8,57 @@ Module.register("MMM-LoLeSports",{
 
 
 		let header = document.createElement("tr");
-		header.classList.add("tr");
+		header.classList.add("tableheader");
 
 		let dataCell = document.createElement("th");
-		dataCell.classList.add("th");
+		dataCell.classList.add("headcell");
 		dataCell.innerHTML = "League";
 		header.appendChild(dataCell);
 
+		let dataCell = document.createElement("th");
+		dataCell.classList.add("headcell");
+		header.appendChild(dataCell);
+
 		dataCell = document.createElement("th");
-		dataCell.classList.add("th");
+		dataCell.classList.add("headcell");
 		dataCell.innerHTML = "Team 1";
 		header.appendChild(dataCell);
 
 		dataCell = document.createElement("th");
-		dataCell.classList.add("th");
+		dataCell.classList.add("headcell");
 		dataCell.innerHTML = "Team 2 ";
 		header .appendChild(dataCell);
 		wrapper.appendChild(header);
 
 
 		let testRow = document.createElement("tr");
-		testRow.classList.add("tr");
 
 		dataCell = document.createElement("td");
-		dataCell.classList.add("td");
+		dataCell.classList.add("league");
 		dataCell.innerHTML = "LEC";
 		testRow.appendChild(dataCell);
 
 		dataCell = document.createElement("td");
-		dataCell.classList.add("td");
+		dataCell.classList.add("team1");
 		dataCell.innerHTML = "Fanatic";
 		testRow.appendChild(dataCell);
 
+		testRow.appendChild(getVersusCell())
+
 		dataCell = document.createElement("td");
-		dataCell.classList.add("td");
+		dataCell.classList.add("team2");
 		dataCell.innerHTML ="G2 Esports";
 		testRow.appendChild(dataCell);
 		wrapper.appendChild(testRow);
 
 		return wrapper;
+	},
+
+	getVersusCell: function(){
+		let versusCell = document.createElement("td");
+		versusCell.classList.add("versuscell");
+		versusCell.innerHTML = "VS"
+		return versusCell;
 	},
 
 	getStyles: function(){
