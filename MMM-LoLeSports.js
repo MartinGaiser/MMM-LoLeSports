e58d2002517ee1ffcb9222e69fe9dc43f0ddab70
Martin Gaiser <martin-gaiser@t-online.de>
2020-06-08 11:24:25 +0200

    innerHTML after elementcreation

--- MMM-LoLeSports.js
+++ MMM-LoLeSports.js
@@ -4,15 +4,15 @@ Module.register("lolesports",{
 		var wrapper = document.createElement("table");
 		wrapper.id = "Wrapper";
 		var header = document.createElement("tr");
-		header.appendChild(document.createElement("th").innerHTML("League"));
-		header.appendChild(document.createElement("th").innerHTML("Team 1"));
-		header.appendChild(document.createElement("th").innerHTML("Team 2"));
+		header.appendChild((document.createElement("th")).innerHTML("League"));
+		header.appendChild((document.createElement("th")).innerHTML("Team 1"));
+		header.appendChild((document.createElement("th")).innerHTML("Team 2"));
 		wrapper.appendChild(header);
 
 		var testRow = document.createElement("tr");
-		header.appendChild(document.createElement("td").innerHTML("LEC"));
-		header.appendChild(document.createElement("td").innerHTML("Fanatic"));
-		header.appendChild(document.createElement("td").innerHTML("G2 Esports"));
+		header.appendChild((document.createElement("td")).innerHTML("LEC"));
+		header.appendChild((document.createElement("td")).innerHTML("Fanatic"));
+		header.appendChild((document.createElement("td")).innerHTML("G2 Esports"));
 		wrapper.appendChild(testRow);
 
 		return wrapper;
