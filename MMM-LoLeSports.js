36ef654702f753de6635affd0aaa11f2741891c1
Martin Gaiser <martin-gaiser@t-online.de>
2020-06-08 13:58:47 +0200

    init packagejson style simplified

--- MMM-LoLeSports.js
+++ MMM-LoLeSports.js
@@ -11,20 +11,20 @@ Module.register("MMM-LoLeSports",{
 		let header = document.createElement("tr");
 
 		let dataCell = document.createElement("th");
-		dataCell.classList.add("leagueheader");
+		dataCell.classList.add("leagueheader", "th");
 		header.appendChild(dataCell);
 
 		dataCell = document.createElement("th");
-		dataCell.classList.add("team1header");
+		dataCell.classList.add("team1header", "th");
 		dataCell.innerHTML = "Team 1";
 		header.appendChild(dataCell);
 
 		dataCell = document.createElement("th");
-		dataCell.classList.add("versusheader");
+		dataCell.classList.add("versusheader","th");
 		header.appendChild(dataCell);
 
 		dataCell = document.createElement("th");
-		dataCell.classList.add("team2header");
+		dataCell.classList.add("team2header","th");
 		dataCell.innerHTML = "Team 2 ";
 		header .appendChild(dataCell);
 		wrapper.appendChild(header);
@@ -33,19 +33,19 @@ Module.register("MMM-LoLeSports",{
 		let testRow = document.createElement("tr");
 
 		dataCell = document.createElement("td");
-		dataCell.classList.add("league");
+		dataCell.classList.add("league","td");
 		dataCell.innerHTML = "LEC";
 		testRow.appendChild(dataCell);
 
 		dataCell = document.createElement("td");
-		dataCell.classList.add("team1");
+		dataCell.classList.add("team1","td");
 		dataCell.innerHTML = "Fanatic";
 		testRow.appendChild(dataCell);
 
 		testRow.appendChild(this.getVersusCell())
 
 		dataCell = document.createElement("td");
-		dataCell.classList.add("team2");
+		dataCell.classList.add("team2","td");
 		dataCell.innerHTML ="G2 Esports";
 		testRow.appendChild(dataCell);
 		wrapper.appendChild(testRow);
@@ -55,13 +55,13 @@ Module.register("MMM-LoLeSports",{
 
 	getVersusCell: function(){
 		let versusCell = document.createElement("td");
-		versusCell.classList.add("versuscell");
+		versusCell.classList.add("versuscell", "td");
 		versusCell.innerHTML = "VS"
 		return versusCell;
 	},
 
 	getStyles: function(){
-		return ['MMM-LoLeSports.css'];
+		return ['MMM-LoLeSports.css','font-awesome.css'];
 	},
 
 	start: function() {
