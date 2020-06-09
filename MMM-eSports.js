bb353bf8a931d0322da7eb55e89d559e8ac378a3
Martin Gaiser <martin-gaiser@t-online.de>
2020-06-09 17:30:51 +0200

    styling

--- MMM-eSports.js
+++ MMM-eSports.js
@@ -52,8 +52,8 @@ Module.register("MMM-eSports", {
 			return wrapper;
 		}else{
 			var wrapper = document.createElement("table");
-			wrapper.classList.add("leaguetable", "small");
-			wrapper.appendChild(this.getHeaderRow());
+			wrapper.classList.add("leaguetable", );
+			//wrapper.appendChild(this.getHeaderRow());
 			for (let i = 0; i < this.leagueData.length; i++){
 				wrapper.appendChild(this.getDataRow(this.leagueData[i].scheduled_at, this.leagueData[i].leagueName, this.leagueData[i].leagueImage,
 					 this.leagueData[i].team1,this.leagueData[i].team1Url, this.leagueData[i].team2, this.leagueData[i].team2Url))
@@ -160,7 +160,7 @@ Module.register("MMM-eSports", {
 
 	getDateCell: function(date){
 		let dataCell = document.createElement("td")
-		dataCell.classList.add("datecell","td");
+		dataCell.classList.add("dateCell","td");
 		dataCell.innerHTML = date;
 		return dataCell;
 	},
